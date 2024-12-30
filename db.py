a25e15533ef545eda1ce96efe195fa3f1812db84
Ani-nem <anunem2@Gmail.com>
2024-12-30 14:29:56 -0800

    Added similarity search, register and add embeddings helper methods for db

--- db.py
+++ db.py
@@ -1,5 +1,6 @@
 from typing import Optional
-from sqlalchemy import create_engine, ForeignKey
+from sqlalchemy import create_engine, ForeignKey, select
+from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import declarative_base, Mapped, mapped_column, relationship, sessionmaker
 from pgvector.sqlalchemy import Vector
 from dotenv import load_dotenv
@@ -50,8 +51,90 @@ Session = sessionmaker(bind=engine)
 def get_db() -> Session:
     try :
         db = Session()
-        yield Session()
+        yield db
     except Exception as e :
         print(f"Error: {str(e)}")
     finally:
         db.close()
+
+def add_embedding(db: Session, embedding: list[float], confidence: float, person : Person):
+    """
+    Adds embedding to the specified person
+    :param db: database session
+    :param embedding: embedding vector
+    :param confidence: confidence value
+    :param person: person
+    :return: Updated person object, None if not found
+    """
+    try:
+        db.add(Embedding(embedding=embedding, person=person, confidence=confidence))
+        db.commit()
+
+        person_statement = select(Person).where(Person.id == person.id)
+        updated_person = db.execute(person_statement).scalars().first()
+        return updated_person
+    except Exception as e :
+        print(f"Error adding embedding to {person.name}: {str(e)}")
+        db.rollback()
+
+def register_person(db: Session, embedding: list[float], confidence: float):
+    """
+    Creates new unknown person with given embedding
+    :param db: database session
+    :param embedding: embedding vector
+    :param confidence: confidence value
+    :return: Newly created Person Object, None if not found
+    """
+    try:
+        #Create new Person
+        new_person = Person()
+        db.add(new_person)
+        db.flush()
+
+        #Create new embedding and link to Person
+        new_embedding = Embedding(embedding=embedding, confidence=confidence, person = new_person)
+        db.add(new_embedding)
+        db.commit()
+
+        person_statement = select(Person).where(Person.id == new_person.id)
+        updated_person = db.execute(person_statement).scalars().first()
+        return updated_person
+    except Exception as e :
+        print(f"Error registering new person: {str(e)}")
+
+def similarity_search(db: Session, orig_embedding : list[float]):
+    """
+    Scans db for closest neighbour embedding of given vector using cosine distance
+    :param db: database session
+    :param orig_embedding: embedding vector
+    :return: the closest embedding, and the person it belongs to, or None for no match.
+    """
+    try:
+        statement = (
+            select(Embedding)
+            .where(Embedding.embedding.cosine_distance(orig_embedding) >= 0.9)
+            .order_by(Embedding.embedding.cosine_distance(orig_embedding))
+            .limit(1))
+        closest_embedding_obj = db.execute(statement).scalars().first()
+
+        if closest_embedding_obj is None:
+            return None, None
+        else:
+            closest_embedding = closest_embedding_obj.embedding
+            closest_person = closest_embedding_obj.person
+            return closest_embedding, closest_person
+
+    except SQLAlchemyError as e :
+        print(f"Database Error occurred during Similarity Search: {str(e)}")
+        return None, None
+    except Exception as e :
+        print(f"An Error occurred during Similarity Search: {str(e)}")
+        return None, None
+
+
+
+
+
+
+
+
