9934dc9b00696f1b27828208341f2106171d32d0
Ani-nem <anunem2@Gmail.com>
2025-02-21 17:11:03 -0800

    Added cors middleware to fastapi backend

--- backend/server.py
+++ backend/server.py
@@ -1,19 +1,31 @@
 from typing import Annotated, Optional, List
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.params import Depends
 from facialrecognition import FaceRecognitionModel
-from database.db import DataBaseOps, DataBaseConnection
+from database.db import DataBaseOps
 from sqlalchemy.orm import Session
 from pydantic import BaseModel, ConfigDict
 from database.db_config import Base, engine, get_db
 
 app = FastAPI()
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://localhost:8080",
+    "http://localhost:8000"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class PersonModel(BaseModel):
     id: int
     name: Optional[str] = None
-    # Note: We're not including embeddings in the response
 
     model_config = ConfigDict(from_attributes=True)
 
--- backend/testing.py
+++ backend/testing.py
@@ -1,16 +1,16 @@
 from facialrecognition import FaceRecognitionModel
 from database.db import DataBaseOps, DataBaseConnection
-from database.db_config import get_db
+from database.db_config import get_db, Base, engine
 
 
 
 
-
+Base.metadata.create_all(engine)
 db_conn = DataBaseConnection()
 
 db_ops = DataBaseOps()
 model = FaceRecognitionModel("yolo11n.pt", ["person"], db_ops)
 
 with next(get_db()) as db:
-    model.detect_people(db, "../datasets/testDataOnePerson")
+    model.detect_people(db, "../datasets/testData")
     model.visualize_results(db)
