16e852c012cc038599c3f1d96edae98442e4ceed
Ani-nem <anunem2@Gmail.com>
2025-02-23 00:50:59 -0800

    Added user authentication configurations in backend

---
+++ backend/auth/auth_config.py
@@ -0,0 +1,32 @@
+from typing import Annotated
+from jose import jwt, JWTError
+from dotenv import load_dotenv
+import os
+
+from fastapi.params import Depends
+from passlib.context import CryptContext
+from fastapi import APIRouter, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+
+load_dotenv()
+SECRET_KEY = os.environ.get("AUTH_SECRET_KEY")
+AUTH_ALGORITHM = os.environ.get("AUTH_ALGORITHM")
+AUTH_ACCESS_EXPIRE_MINUTES = int(os.environ.get("AUTH_ACCESS_EXPIRE_MINUTES"))
+
+router = APIRouter(prefix="/auth", tags=["auth"])
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl="token")
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_bearer_dependency = Annotated[str, Depends(oauth2_bearer)]
+
+async def get_current_user(token: oauth2_bearer_dependency):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[AUTH_ALGORITHM])
+        email: str = payload.get("sub")
+        user_id: int = payload.get("id")
+        if email is None or user_id is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate user credentials")
+        return {"email": email, "id": user_id}
+    except JWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate user credentials")
+
+user_dependency = Annotated[dict, Depends(get_current_user)]
