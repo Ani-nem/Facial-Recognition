6ef8a359d1aaa340893cf376199ce30dbd73fa50
Ani-nem <anunem2@Gmail.com>
2025-03-30 15:45:09 -0700

    Fixed db.py to search through all people associated to user

--- backend/database/db.py
+++ backend/database/db.py
@@ -5,10 +5,12 @@ from sqlalchemy.orm import sessionmaker, Session
 from .db_config import engine
 from .models import Person, Embedding, User
 
-#TODO: remove
+
+# TODO: remove
 class DataBaseConnection:
     def __init__(self):
         self.Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
     def get_db(self) -> Generator[Session, Any, None]:
         try:
             db = self.Session()
@@ -18,9 +20,10 @@ class DataBaseConnection:
         finally:
             db.close()
 
+
 class DataBaseOps:
     @staticmethod
-    def add_embedding(db: Session, embedding: list[float], confidence: float, img_path: str, person_id : int):
+    def add_embedding(db: Session, embedding: list[float], confidence: float, img_path: str, person_id: int):
         """
         Adds embedding to the specified person
         :param db: database session
@@ -31,7 +34,7 @@ class DataBaseOps:
         :return: Updated person object, None if not found
         """
         try:
-            #Add embedding to given person
+            # Add embedding to given person
             # noinspection PyTypeChecker
             db.add(Embedding(embedding=embedding, person_id=person_id, img_path=img_path, confidence=confidence))
             db.commit()
@@ -39,12 +42,12 @@ class DataBaseOps:
             person_statement = select(Person).where(Person.id == person_id)
             updated_person = db.execute(person_statement).scalars().first()
             return updated_person
-        except Exception as e :
+        except Exception as e:
             print(f"Error adding embedding to person {person_id}: {str(e)}")
             db.rollback()
 
     @staticmethod
-    def register_person(db: Session, embedding: list[float], img_path : str, confidence: float = 1.0):
+    def register_person(db: Session, embedding: list[float], img_path: str, confidence: float = 1.0):
         """
         Creates new unknown person with given embedding
         :param db: database session
@@ -54,26 +57,26 @@ class DataBaseOps:
         :return: Newly created Person Object
         """
         try:
-            #Create new Person
+            # Create new Person
             new_person = Person()
             db.add(new_person)
             db.flush()
 
-            #Create new embedding and link to Person
+            # Create new embedding and link to Person
             # noinspection PyTypeChecker
-            new_embedding = Embedding(embedding=embedding, confidence=confidence, img_path=img_path, person = new_person)
+            new_embedding = Embedding(embedding=embedding, confidence=confidence, img_path=img_path, person=new_person)
             db.add(new_embedding)
             db.commit()
 
             person_statement = select(Person).where(Person.id == new_person.id)
             updated_person = db.execute(person_statement).scalars().first()
             return updated_person
-        except Exception as e :
+        except Exception as e:
             print(f"Error registering new person: {str(e)}")
             db.rollback()
 
     @staticmethod
-    def similarity_search(db: Session, orig_embedding : list[float], user_id: int):
+    def similarity_search(db: Session, orig_embedding: list[float], user_id: int):
         """
         Scans db for closest neighbour embedding of given vector using cosine distance
         :param db: database session
@@ -82,16 +85,16 @@ class DataBaseOps:
         """
         try:
 
-            #Find similar embedding
+            #find valid people to look through for given user
+            people_to_search = db.execute(select(Person.id).where(Person.user_id == user_id)).scalars().all()
+            # Find similar embedding
             statement = (
                 select(
                     Embedding,
                     Embedding.embedding.cosine_distance(orig_embedding)
                 )
-                .where(Embedding.embedding.cosine_distance(orig_embedding) <= 0.0558,
-                       Embedding.person_id == (select(Person.id)
-                                               .where(Person.user_id == user_id)
-                                               .scalar_subquery()))
+                .where(Embedding.embedding.cosine_distance(orig_embedding) <= 0.0658,
+                       Embedding.person_id.in_(people_to_search))
                 .order_by(Embedding.embedding.cosine_distance(orig_embedding))
                 .limit(1))
             result = db.execute(statement).first()
@@ -100,15 +103,15 @@ class DataBaseOps:
                 return None, None, None
             else:
                 closest_embedding_obj, distance = result
-                similarity = 1-distance
+                similarity = 1 - distance
                 closest_embedding = closest_embedding_obj.embedding
                 closest_person_id = closest_embedding_obj.person.id
                 return closest_embedding, closest_person_id, similarity
 
-        except SQLAlchemyError as e :
+        except SQLAlchemyError as e:
             print(f"Database Error occurred during Similarity Search: {str(e)}")
             return None, None, None
-        except Exception as e :
+        except Exception as e:
             print(f"An Error occurred during Similarity Search: {str(e)}")
             return None, None, None
 
@@ -128,7 +131,7 @@ class DataBaseOps:
             return []
 
     @staticmethod
-    def create_person(db: Session, user_id:int):
+    def create_person(db: Session, user_id: int):
         try:
             new_person = Person(user_id=user_id)
             db.add(new_person)
@@ -138,5 +141,3 @@ class DataBaseOps:
         except Exception as e:
             print(f"Error creating new person: {str(e)}")
             db.rollback()
-
-
